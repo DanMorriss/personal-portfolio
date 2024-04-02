eff05b9dd4ff36f31f1b845f0f68f715f9663811
Dan Morriss <danielmorriss1@gmail.com>
2024-04-02 14:31:47 +0100

    Initial commit

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dan Morriss</h1>
+      <p>Full Stack Developer</p>
+    </div>
+  );
+}
+
+export default App;
