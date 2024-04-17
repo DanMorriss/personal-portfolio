9641d7f36ecd0eb88e855145ad8655174ea93d71
Dan Morriss <danielmorriss1@gmail.com>
2024-04-17 23:54:29 +0100

    Add retro mashup mayhem

--- personal-portfolio/src/pages/landing-desktop.jsx
+++ personal-portfolio/src/pages/landing-desktop.jsx
@@ -20,11 +20,18 @@ import git_logo from "../public/images/git-logo.png";
 import highlights_mobile from "../public/images/projects/highlights-mobile.png";
 import highlights_tablet from "../public/images/projects/highlights-tablet.png";
 import highlights_laptop from "../public/images/projects/highlights-laptop.png";
+import highlights_desktop from "../public/images/projects/highlights-desktop.png";
+import barbershop_mobile from "../public/images/projects/barbershop-mobile.png";
+import barbershop_tablet from "../public/images/projects/barbershop-tablet.png";
+import barbershop_laptop from "../public/images/projects/barbershop-laptop.png";
+import barbershop_desktop from "../public/images/projects/barbershop-desktop.png";
+import retro_mashup_mayhem_laptop from "../public/images/projects/retro-mashup-mayhem-laptop.png";
+import retro_mashup_mayhem_desktop from "../public/images/projects/retro-mashup-mayhem-desktop.png";
 
 function LandingDesktop() {
   return (
     <div className="App">
-      <Parallax pages={5}>
+      <Parallax pages={8}>
         <Hero />
         {/* About Section */}
         <div className="about-container">
@@ -134,12 +141,13 @@ function LandingDesktop() {
         </ParallaxLayer>
 
         {/* Projects Section */}
+        {/* Highlights */}
 
         <ParallaxLayer offset={3} speed={3.7}>
           <Image
             src={highlights_mobile}
             alt="mobile view of the highlights site"
-            className="highlights-mobile"
+            className="mobile-mockup"
           />
         </ParallaxLayer>
 
@@ -147,28 +155,112 @@ function LandingDesktop() {
           <Image
             src={highlights_tablet}
             alt="tablet view of the highlights site"
-            className="highlights-tablet"
+            className="tablet-mockup"
           />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={3} speed={1.7}>
+        <ParallaxLayer offset={3} speed={1.3}>
           <Image
             src={highlights_laptop}
             alt="laptop view of the highlights site"
-            className="highlights-laptop"
+            className="laptop-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} speed={2.7}>
+          <Image
+            src={highlights_desktop}
+            alt="desktop view of the highlights site"
+            className="desktop-mockup"
           />
         </ParallaxLayer>
 
         <ParallaxLayer sticky={{ start: 2.5, end: 3 }}>
           <div className="project-description">
             <h1>Highlights</h1>
-            <h2>React, Django REST Framework, Heroku</h2>
+            <h2>Social Network</h2>
             <p>
-              A social media platform designed to cultivate gratitude and
-              positivity in everyday life. Inspired by the principles of the
-              Five Minute Journal, the platform provides users with a space to
-              share the highlights of their day and connect with others in a
-              supportive online community.
+              A photo sharing social media platform. Inspired by the principles
+              of the Five Minute Journal, the platform provides users with a
+              space to share the highlights of their day and connect with other.
+            </p>
+          </div>
+        </ParallaxLayer>
+
+        {/* Nick's Barbershop */}
+
+        <ParallaxLayer offset={4.5} speed={3.7}>
+          <Image
+            src={barbershop_mobile}
+            alt="mobile view of the barbershop site"
+            className="mobile-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4.5} speed={0.9}>
+          <Image
+            src={barbershop_tablet}
+            alt="tablet view of the barbershop site"
+            className="tablet-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4.5} speed={1.3}>
+          <Image
+            src={barbershop_laptop}
+            alt="laptop view of the barbershop site"
+            className="laptop-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4.5} speed={2.7}>
+          <Image
+            src={barbershop_desktop}
+            alt="desktop view of the barbershop site"
+            className="desktop-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer sticky={{ start: 4.1, end: 4.4 }}>
+          <div className="project-description">
+            <h1>Nick's Barbershop</h1>
+            <h2>Website & Booking System</h2>
+            <p>
+              A website with a booking system that allows customers to book,
+              edit and cancel appointments with email conformation. Including an
+              admin panel for barbers to manage appointments and contact
+              customers.
+            </p>
+          </div>
+        </ParallaxLayer>
+
+        {/* Retro Mashup Mayhem */}
+
+        <ParallaxLayer offset={5.9} speed={0.3}>
+          <Image
+            src={retro_mashup_mayhem_laptop}
+            alt="laptop view of retro mashup mayhem"
+            className="retro-laptop-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={5.9} speed={0.6}>
+          <Image
+            src={retro_mashup_mayhem_desktop}
+            alt="desktop view of retro mashup mayhem"
+            className="retro-desktop-mockup"
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer sticky={{ start: 5.3, end: 6 }}>
+          <div className="project-description">
+            <h1>Retro Mashup Mayhem</h1>
+            <h2>Classic Retro Platform Game</h2>
+            <p>
+              The pixel art style, synthesized sound effects, and challenging
+              game-play harken back to the golden age of arcade games. Engaging
+              game mechanics, a quest for lost heroes and a trophy collection
+              offer players an authentic and thrilling experience.
             </p>
           </div>
         </ParallaxLayer>
